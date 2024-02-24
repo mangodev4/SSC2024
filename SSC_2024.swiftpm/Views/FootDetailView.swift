a82635d4562b9b49c323b6ac7669f28e94459759
Mango <mangodev4@gmail.com>
2024-02-25 03:31:13 +0900

    feat. body button navigation 구현
    버튼 클릭시 각각 할당된 뷰로 이동 구현

---
+++ SSC_2024.swiftpm/Views/FootDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FootDetailView.swift
+//  SSC_2024
+//
+//  Created by Yujin Son on 2/24/24.
+//
+
+import SwiftUI
+
+struct FootDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FootDetailView()
+}
